928f8842f57f67c70a89c38de1be2dffddb3eb2d
drew <43808240+drew458@users.noreply.github.com>
2022-05-22 19:14:53 +0200

    another auth refactor

--- catering/src/main/java/it/uniroma3/siw/auth/model/Credentials.java
+++ catering/src/main/java/it/uniroma3/siw/auth/model/Credentials.java
@@ -8,10 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
-import lombok.Data;
-
 @Entity
-public @Data class Credentials {
+public class Credentials {
 
 	public static final String DEFAULT_ROLE = "DEFAULT";
 	public static final String ADMIN_ROLE = "ADMIN";
@@ -31,4 +29,67 @@ public @Data class Credentials {
 	
 	@OneToOne(cascade = CascadeType.ALL)
 	private User user;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public Credentials() {	}
+
+	public Credentials(Long id, String username, String password, String role, User user) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Credentials that = (Credentials) obj;
+		return this.id.equals(that.getId());
+	}
+	
+	
 }
--- catering/src/main/java/it/uniroma3/siw/auth/model/User.java
+++ catering/src/main/java/it/uniroma3/siw/auth/model/User.java
@@ -7,11 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-
 @Entity
 @Table(name = "users") // In postgres user e' una parola riservata
-public @Data class User {
+public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,4 +27,67 @@ public @Data class User {
 	@Column(nullable=false)
 	private String password;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User() {	}
+
+	public User(Long id, String nome, String cognome, String username, String password) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		User that = (User) obj;
+		return this.id.equals(that.getId());
+	}
+	
+	
 }
