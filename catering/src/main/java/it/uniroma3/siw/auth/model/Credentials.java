1b76e7df79a02217c54fdcc3c5df45e30c95978a
drew <43808240+drew458@users.noreply.github.com>
2022-05-22 18:41:14 +0200

    refactored auth flow

---
+++ catering/src/main/java/it/uniroma3/siw/auth/model/Credentials.java
@@ -0,0 +1,34 @@
+package it.uniroma3.siw.auth.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+
+@Entity
+public @Data class Credentials {
+
+	public static final String DEFAULT_ROLE = "DEFAULT";
+	public static final String ADMIN_ROLE = "ADMIN";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private String role;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private User user;
+}
--- catering/src/main/java/it/uniroma3/siw/auth/model/User.java
+++ catering/src/main/java/it/uniroma3/siw/auth/model/User.java
@@ -1,72 +1,32 @@
 package it.uniroma3.siw.auth.model;
 
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
 
 @Entity
-public class User {
+@Table(name = "users") // In postgres user e' una parola riservata
+public @Data class User {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-
-	@Column(nullable = false, unique = true, length = 45)
-	private String email;
-
-	@Column(nullable = false, length = 64)
+	
+	@Column(nullable=false)
+	private String nome;
+	
+	@Column(nullable=false)
+	private String cognome;
+	
+	@Column(nullable=false)
+	private String username;
+	
+	@Column(nullable=false)
 	private String password;
-
-	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	private Set<Role> roles;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Set<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
-
-	public User() {	}
-
-	public User(Long id, String email, String password, Set<Role> roles) {
-		super();
-		this.id = id;
-		this.email = email;
-		this.password = password;
-		this.roles = roles;
-	}
 
 }
