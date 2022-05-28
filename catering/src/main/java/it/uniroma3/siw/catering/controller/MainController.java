7ca8d9e7585d89ec42efba4184d6ec6bbd14a8dc
drew <43808240+drew458@users.noreply.github.com>
2022-05-28 18:20:51 +0200

    login and signup changes

--- catering/src/main/java/it/uniroma3/siw/catering/controller/MainController.java
+++ catering/src/main/java/it/uniroma3/siw/catering/controller/MainController.java
@@ -1,23 +1,16 @@
 package it.uniroma3.siw.catering.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import it.uniroma3.siw.catering.model.User;
-import it.uniroma3.siw.catering.repository.UserRepository;
 import it.uniroma3.siw.catering.service.BuffetService;
 import it.uniroma3.siw.catering.service.ChefService;
 import it.uniroma3.siw.catering.service.PiattoService;
 
 @Controller
 public class MainController {
-
-	@Autowired
-	private UserRepository repository;
 	
 	@Autowired
 	private ChefService chefService;
@@ -31,27 +24,6 @@ public class MainController {
 	@GetMapping(value = {"/", "index"})
 	public String index(Model model) {
 		return "index";
-	}
-
-	@PostMapping("/login")
-	public String login(User user) { 
-	    return "login";
-	}
-
-	@GetMapping("/signup")
-	public String showSignupForm(Model model) {
-		model.addAttribute("user", new User());
-		return "signup_form";
-	}
-	
-	@PostMapping("/signup_process")
-	public String processSignup(User user) {
-	    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-	    String encodedPassword = passwordEncoder.encode(user.getPassword());
-	    user.setPassword(encodedPassword); 
-	    repository.save(user);
-	     
-	    return "signup_success";
 	}
 	
 	@GetMapping("/chef")
